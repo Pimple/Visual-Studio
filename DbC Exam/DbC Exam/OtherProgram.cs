a21f891f1f4bf4c2c509e9af86240b89bd0e673c
Pimple <ferociouscookiemonster@gmail.com>
2015-06-02 12:53:53 +0200

    DbC Exam done

---
+++ DbC Exam/DbC Exam/OtherProgram.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbC_Exam
+{
+	class OtherProgram
+	{
+		static void Main(string[] args)
+		{
+			Element<string>[] array = new Element<string>[10];
+			array[0] = new Element<string>("Element 0", Color.RED);
+			array[1] = new Element<string>("Element 1", Color.WHITE);
+			array[2] = new Element<string>("Element 2", Color.GREEN);
+			array[3] = new Element<string>("Element 3", Color.WHITE);
+			array[4] = new Element<string>("Element 4", Color.GREEN);
+			array[5] = new Element<string>("Element 5", Color.RED);
+			array[6] = new Element<string>("Element 6", Color.GREEN);
+			array[7] = new Element<string>("Element 7", Color.WHITE);
+			array[8] = new Element<string>("Element 8", Color.RED);
+			array[9] = new Element<string>("Element 9", Color.WHITE);
+
+			foreach (Element<string> element in array)
+				Console.Out.WriteLine(element.Color + "\t" + element.Value);
+
+			HerpDerpSort(array);
+			Console.Out.WriteLine("\nSorting array...\n");
+
+			foreach (Element<string> element in array)
+				Console.Out.WriteLine(element.Color + "\t" + element.Value);
+		}
+		
+		private static void HerpDerpSort(Element<string>[] array)
+		{
+			Contract.Requires(array != null);
+			Contract.Ensures(IsSorted(array));
+
+			int i = 0;
+			int lengthA = array.Length;
+
+			Contract.Assert(i >= 0 && i <= array.Length);
+			Contract.Assert(array.Length == lengthA);
+
+			while (i < array.Length)
+			{
+				int j = i + 1;
+				int lengthB = array.Length;
+
+				Contract.Assert(j > i && j <= array.Length);
+				Contract.Assert(lengthB == array.Length);
+
+				while (j < array.Length)
+				{
+					if (White(array[i]) && Green(array[j]))
+					{
+						Element<string> temp = array[i];
+						array[i] = array[j];
+						array[j] = temp;
+						break;
+					}
+					else if (Red(array[i]) && Green(array[j]) || White(array[j]))
+					{
+						Element<string> temp = array[i];
+						array[i] = array[j];
+						array[j] = temp;
+					}
+					j++;
+					Contract.Assert(j > i && j <= array.Length);
+					Contract.Assert(lengthB == array.Length);
+				}
+				i++;
+				Contract.Assert(i >= 0 && i <= array.Length);
+				Contract.Assert(array.Length == lengthA);
+			}
+		}
+
+		[Pure]
+		private static bool IsSorted(Element<string>[] array)
+		{
+			bool sorted = true;
+
+			for (int i = 1; i < array.Length - 1; i++)
+				if (Green(array[i]) && (White(array[i-1]) || Red(array[i-1])))
+					sorted = false;
+				else if (White(array[i]) && (Red(array[i-1]) || Green(array[i+1])))
+					sorted = false;
+				else if (Red(array[i]) && (White(array[i+1]) || Green(array[i+1])))
+					sorted = false;
+			
+			return sorted;
+		}
+
+		[Pure]
+		private static bool Green(Element<string> element)
+		{
+			Contract.Requires(element != null);
+			return element.Color == Color.GREEN;
+		}
+		[Pure]
+		private static bool White(Element<string> element)
+		{
+			Contract.Requires(element != null);
+			return element.Color == Color.WHITE;
+		}
+		[Pure]
+		private static bool Red(Element<string> element)
+		{
+			Contract.Requires(element != null);
+			return element.Color == Color.RED;
+		}
+	}
+
+	public enum Color
+	{
+		GREEN, RED, WHITE
+	}
+
+	public class Element<T>
+	{
+		public T Value { get; set; }
+		public Color Color { get; set; }
+
+		public Element(T value, Color color)
+		{
+			Contract.Ensures(Value.Equals(value));
+			Contract.Ensures(Color.Equals(color));
+			Value = value;
+			Color = color;
+		}
+	}
+}
---
+++ DbC Exam/DbC Exam/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbC_Exam
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PriorityQueue<string> queue = new PriorityQueue<string>();
+			
+			queue.Insert("Dasher", 4);
+			queue.Insert("Dancer", 2);
+			queue.Insert("Prancer", 8);
+			queue.Insert("Vixen", 5);
+			queue.Insert("Comet", 5);
+			queue.Insert("Cupid", 6);
+			queue.Insert("Dunder", 4);
+			queue.Insert("Blixem", 1);
+			queue.Insert("Rudolph", 9001);
+			queue.Insert("Santa", 0);
+
+			int deletions = 5;
+			for (int i = 0; i < deletions; i++)
+				queue.Remove();
+
+			queue.PrintQueue();
+		}
+	}
+
+	public class PriorityQueue<T>
+	{
+		private Item<T>[] items;
+		private int quantity;
+
+		public PriorityQueue()
+		{
+			Contract.Ensures(quantity == 0);
+			Contract.Ensures(items != null && items.Length == 10);
+
+			items = new Item<T>[10];
+			quantity = 0;
+		}
+
+		private class Item<E>
+		{
+			public int Priority { get; set; }
+			public E Value { get; set; }
+
+			public Item(E value, int priority)
+			{
+				Contract.Requires(value != null);
+				Contract.Ensures(Value.Equals(value));
+				Contract.Ensures(Priority == priority);
+				Priority = priority;
+				Value = value;
+			}
+		}
+
+		public void Insert(T newValue, int priority)
+		{
+			// it would probably be a good idea for newValue not to be null, but 
+			// the way I have implemented it, one could just add a null value with 
+			// a priority... Since priority is an int and ints are easily 
+			// comparable, it doesn't matter what int is specified.
+
+			Contract.Ensures(quantity == Contract.OldValue<int>(quantity) + 1);
+			Contract.Ensures(items[quantity-1].Value.Equals(newValue) 
+				&& items[quantity-1].Priority == priority);
+
+			if (items.Length == quantity)
+			{
+				Item<T>[] oldNumbers = items;
+				items = new Item<T>[quantity * 2];
+				for (int i = 0; i < quantity; i++)
+					items[i] = oldNumbers[i];
+			}
+			items[quantity] = new Item<T>(newValue, priority);
+			
+			quantity++;
+		}
+
+		public void Remove()
+		{
+			// If empty, you can still run this but it won't do anything. Therefore, 
+			// it requires nothing that isn't ensured by the constructor.
+
+			Contract.Ensures(quantity == Contract.OldValue<int>(quantity) - 1 || quantity == 0);
+
+			if (quantity > 0)
+			{
+				int indexToRemove = -1;
+				for (int i = 0; i < quantity; i++)
+					if (indexToRemove == -1 && items[i] != null)
+						indexToRemove = i;
+					else if (items[i] != null)
+						if (items[indexToRemove].Priority < items[i].Priority)
+							indexToRemove = i;
+				items[indexToRemove] = null;
+
+				// Defragmentation
+				for (int i = indexToRemove; i < quantity-1; i++)
+				{
+					items[i] = items[i+1];
+					items[i+1] = null;
+				}
+				if (quantity > 0)
+					quantity--;
+			}
+		}
+
+		[Pure]
+		public bool IsEmpty()
+		{
+			bool isEmpty = true;
+			foreach (Item<T> item in items)
+				if (item != null)
+					return false;
+			return isEmpty;
+		}
+
+		[Pure]
+		public void PrintQueue()
+		{
+			if (quantity > 0)
+			{
+				foreach (Item<T> item in items)
+					if (item != null)
+						Console.Out.WriteLine(item.Priority + "\t" + item.Value);
+			}
+			else
+				Console.Out.WriteLine("The queue is empty.");
+		}
+
+		[ContractInvariantMethod]
+		private void ObjectInvariant()
+		{
+			Contract.Invariant(quantity >= 0);
+			Contract.Invariant(items != null);
+			Contract.Invariant(items.Length >= quantity);
+		}
+	}
+}
